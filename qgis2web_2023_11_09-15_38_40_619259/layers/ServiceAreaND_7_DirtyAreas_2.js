f24696b2b2e6bb9fb2f9506deda3d0e0aa4e55aa
raiihanramadhan18 <150319572+raiihanramadhan18@users.noreply.github.com>
2023-11-09 16:34:46 +0700

    Delete qgis2web_2023_11_09-15_38_40_619259 directory

--- qgis2web_2023_11_09-15_38_40_619259/layers/ServiceAreaND_7_DirtyAreas_2.js
+++
@@ -1,1 +0,0 @@
-var json_ServiceAreaND_7_DirtyAreas_2 = {"type":"FeatureCollection","name":"ServiceAreaND_7_DirtyAreas_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"ObjectID":"1","IsRetired":0.0,"DirtyArea_Length":0.0,"DirtyArea_Area":0.0},"geometry":{"type":"MultiPolygon","coordinates":[[]]}}]}
